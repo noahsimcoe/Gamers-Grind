04886cb1533c4f8c5ee631116a8ce399ba99486a
bcm22 <135455152+bcm22@users.noreply.github.com>
2023-09-20 21:27:41 -0500

    fixed connection error

--- Main/server.js
+++ Main/server.js
@@ -39,11 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
-
-app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
-  sequelize.sync({ force: false })
+sequelize.sync({ force: false }).then(() => {
+app.listen(PORT, () => console.log('Now listening'));
 });
+
