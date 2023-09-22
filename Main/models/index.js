ac2bd5c258f5772f68bce1c8a29f9775819832a8
Jaxson20 <jaxsonfellhauerschool@gmail.com>
2023-09-21 19:15:34 -0500

    index.js for models

---
+++ Main/models/index.js
@@ -0,0 +1,41 @@
+const Sequelize = require('sequelize');
+const sequelize = require('./config/connection');
+
+
+const User = require('./models/User');
+const CalendarDay = require('./models/CalendarDay');
+const Event = require('./models/Event');
+const HighlightsBoard = require('./models/HighlightsBoard');
+
+User.hasMany(Event, {
+  foreignKey: 'user_id',
+});
+
+CalendarDay.hasMany(Event, {
+  foreignKey: 'day_id',
+});
+
+User.hasMany(HighlightsBoard, {
+  foreignKey: 'user_id',
+});
+
+Event.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Event.belongsTo(CalendarDay, {
+  foreignKey: 'day_id',
+});
+
+HighlightsBoard.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+
+module.exports = {
+  sequelize,
+  User,
+  CalendarDay,
+  Event,
+  HighlightsBoard,
+};
