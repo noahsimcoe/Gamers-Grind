bb7487c294345c4d372cb201c0e87dde467e481b
noahsimcoe <109931528+noahsimcoe@users.noreply.github.com>
2023-09-28 18:50:06 -0500

    got rid of calendar stuff-not being used

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,11 +2,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const eventRoutes = require('./eventRoutes');
 const highlightsRoutes = require('./highlightsRoutes');
-const calendarRoutes = require('./calendarRoutes');
 
 router.use('/users', userRoutes);
 router.use('/events', eventRoutes);
 router.use('/highlights', highlightsRoutes);
-router.use('/calendar', calendarRoutes);
 
 module.exports = router;
